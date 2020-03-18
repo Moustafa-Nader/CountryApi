c84d4b02bb007c8d32bcf1f221eac5120a93c51d
OVGB <lan12131999@gmail.com>
2020-03-18 23:32:04 +0200

    standard tests

--- getcountrydata.py
+++ getcountrydata.py
@@ -3,8 +3,7 @@ import requests
 def getdata(country):
 	url = "https://restcountries.eu/rest/v2/name/" + country
 	request = requests.get(url)
+	if request.status_code == 404:
+		return None
 	jsonObj = request.json()
 	return jsonObj[0]
-
-def setstring():
-	return "aa"
--- requesthandler.py
+++ requesthandler.py
@@ -1,10 +1,12 @@
 from flask_restful import Resource 
 import flask_restful.reqparse as parserrequest
 import getcountrydata
+
 class requesthandler(Resource):
-    
     def get(self,country ):
         obj = getcountrydata.getdata(country)
+        if obj == None:
+            return "404 Country Not found"
         parsedQuery = ParseQuery()
         if parsedQuery == None :
             return obj
@@ -17,11 +19,10 @@ class requesthandler(Resource):
                 result[info] = "invalid"
         return result
 
+
 def ParseQuery():        
         parser = parserrequest.RequestParser()
         parser.add_argument("info")  
         args = 	parser.parse_args()
         return args["info"]
-
-        
 
