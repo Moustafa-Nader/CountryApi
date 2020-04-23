1644abf324c3d870eaf007cbd711b88900bf1cc3
Moustafa-Nader <moustafa.mohamed7979@gmail.com>
2020-04-23 22:20:56 +0200

    Modified coverage script calling

--- Tests/Unit.py
+++ Tests/Unit.py
@@ -3,19 +3,36 @@ import requests
 import sys
 sys.path.append("..")
 import getcountrydata
+import requesthandler
 
 
 class getcountrydatapyTest(unittest.TestCase):
-	def test_getcountrydata(self):
-		url = "https://restcountries.eu/rest/v2/name/egypt"
-		request = requests.get(url)
-		jsonObj = request.json()
-		self.assertEqual(getcountrydata.getdata("egypt"),jsonObj[0])
+    def test_getcountrydata(self):
+        url = "https://restcountries.eu/rest/v2/name/egypt"
+        request = requests.get(url)
+        jsonObj = request.json()
+        self.assertEqual(getcountrydata.getdata("egypt"),jsonObj[0])
 
-	def test_getfakecountrydata(self):
-		self.assertEqual(getcountrydata.getdata("dsaefaeaeda"),None)
-	
-											
+    def test_getfakecountrydata(self):
+        self.assertEqual(getcountrydata.getdata("dsaefaeaeda"),None)
+
+    def test_CheckNullObjtrue(self):
+        self.assertEqual(requesthandler.CheckNullObj(None),True)
+
+    def test_CheckNullObjfalse(self):
+        self.assertEqual(requesthandler.CheckNullObj("None"),False)
+
+    def test_CheckNullQuerytrue(self):
+        self.assertEqual(requesthandler.CheckNullQuery(None),True)
+
+    def test_CheckNullQueryfalse(self):
+        self.assertEqual(requesthandler.CheckNullQuery("None"),False)
+
+    #def test_ParseQuery(self):
+
+
+                                            
 #class requesthandlerTest(unittest.TestCase):
-	#def test_parseQuery(self):
-unittest.main()
+    #def test_parseQuery(self):
+if __name__ == '__main__':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
